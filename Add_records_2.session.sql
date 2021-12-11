2fcf80783b8344416ea22e359d13e00edc9d8b45
Edgar Ehrenberg <ainshin@its.jnj.com>
2021-12-11 18:21:05 +0300

    Updated

---
+++ Add_records_2.session.sql
@@ -0,0 +1,9 @@
+-- INSERT INTO employers (company_name, company_address, yearly_revenue, is_hiring) VALUES ('Tesla', 'Palo Alto, California, USA', 0.87, FALSE);
+
+-- INSERT INTO employers (company_name, company_address, yearly_revenue, is_hiring) VALUES ('Apple', 'Cupertino, California, USA', 0.97, TRUE);
+
+-- INSERT INTO employers (company_name, company_address, yearly_revenue, is_hiring) VALUES ('Amazon', 'Seattle, Washington, USA', 386.8, TRUE);
+
+INSERT INTO conversation (user_name, employer_name, message, date_sent) VALUES (
+    'Big K.R.I.T.', 'Apple', 'Hi, we are great company', '2021-10-16 15:41:29'
+)
